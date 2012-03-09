aae68affad9b9e952b44f33948b668fae6ab411a
Mariot Chauvin <mariot.chauvin@gmail.com>
2012-03-09 15:53:25 +0100

    Compile only for java files
    This commit modifies the controller to compile only java files.

--- app/controllers/Solar.java
+++ app/controllers/Solar.java
@@ -31,12 +31,6 @@ public class Solar extends Controller {
 	public static void list(String path) {
                 List<File> files = Arrays.asList(Play.getFile(path).listFiles());
                 render("index.html", path, files);
-
-        /*try {
-        	renderText(serialize(fileList).getBytes("utf-8"));
-        } catch (UnsupportedEncodingException e) {
-        	//do nothing 
-        }*/
 	}
 
 	public static void edit(String path) {
@@ -53,34 +47,46 @@ public class Solar extends Controller {
 	}
 
 	public static void compile() {
+                String path = params.get("path");
                 String source = params.get("source");
-		ApplicationClass applicationClass = Play.classes.getApplicationClass("controllers.Application");
-                if (applicationClass != null) {
-        	       try {
-            	        applicationClass.refresh();
-                        /* the magic is here :) */
-               	        applicationClass.javaSource = source;
-                        applicationClass.compile();
+
+                if (path.endsWith(".java")) {
+                        javaCompile(source, path);
+                } else {
                         boolean compile = true;
                         renderTemplate("compile.json", compile, 0, 0, 0, "");
-           	        } catch (CompilationException e) {
+                }
+                        
+        }
+
+        private static void javaCompile(String source, String path) {
+                String dotPath = path.replaceAll("/", ".");
+                int offset = dotPath.indexOf("app.");
+                if (offset == 0) {
+                        /* remove app. prefix */
+                        String classPath = dotPath.substring(4);
+                        /* remove trailing .java */
+                        classPath = classPath.substring(0, classPath.length()-5);
+
+                        ApplicationClass applicationClass = Play.classes.getApplicationClass(classPath);
+                        if (applicationClass != null) {
+                        try {
+                                applicationClass.refresh();
+                                /* the magic is here :) */
+                                applicationClass.javaSource = source;
+                                applicationClass.compile();
+                                boolean compile = true;
+                                renderTemplate("compile.json", compile, 0, 0, 0, "");
+                        } catch (CompilationException e) {
                           int errorLine = e.getLineNumber();
                           int srcStart = e.getSourceStart();
                           int srcEnd = e.getSourceEnd();
                           String msg = e.getMessage();
                           boolean compile = false;
-                        renderTemplate("compile.json", compile, errorLine, srcStart, srcEnd, msg);
-            }          
-    	} 
+                          renderTemplate("compile.json", compile, errorLine, srcStart, srcEnd, msg);
+                        }          
+                        }
+                } 
 	}
-
-	private static String serialize(List<File> list) {
-        GsonBuilder gson = new GsonBuilder();
-        Type listFileType = new TypeToken<List<File>>(){}.getType();
-        gson.registerTypeAdapter(File.class, new FileSerializer(listFileType));
-        gson.registerTypeAdapter(listFileType, new FileListSerializer());
-        String result = gson.create().toJson(list, listFileType);
-        return result;
-    }
 
 }
